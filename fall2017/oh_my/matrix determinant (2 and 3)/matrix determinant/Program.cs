be9229728b2e8aa3404bdc8d6a9b7339b595eee9
Don Macaron <donmacaron95@gmail.com>
2017-10-12 08:32:12 +0300

    всё

---
+++ fall2017/oh_my/matrix determinant (2 and 3)/matrix determinant/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix_determinant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Determinant of matrix\nEnter the rank of matrix (2 or 3)...");
+            int rank = int.Parse(Console.ReadLine());
+            int[,] m = new int[rank, rank];
+            int det = 0;
+            Console.WriteLine("Enter your matrix (line by line with spaces)");
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                string enterString = Console.ReadLine();
+                string[] massiveString = enterString.Split(new Char[] { ' ' });
+                for (int j = 0; j < massiveString.Length; j++)
+                {
+                    m[i, j] = int.Parse(massiveString[j]);
+                }
+            }
+
+            switch (rank)
+            {
+                case 2:
+                    det = ThirdOder(m);
+                    Console.WriteLine("Determinant of 2x2 matrix is... {0}", (det));
+                    break;
+                case 3:
+                    det = SecondOrder(m);
+                    Console.WriteLine("Determinant of 3x3 matrix is... {0}",(det));
+                    break;
+                default:
+                    Console.WriteLine("Maybe later.. see you space cowboy..");
+                    break;
+            }
+            
+            Console.ReadKey();
+        }
+
+        static int ThirdOder(int[,]m)
+        {
+            int det = 0;
+            Console.WriteLine("");
+            det = (m[0, 0] * m[1, 1] * m[2, 2] + m[0, 1] * m[1, 2] * m[2, 0] + m[0, 2] * m[1, 0] * m[2, 1]) - (m[0, 2] * m[1, 1] * m[2, 0] + m[0, 0] * m[1, 2] * m[2, 1] + m[0, 1] * m[1, 0] * m[2, 2]);
+            return det;
+        }
+        static int SecondOrder(int[,]m)
+        {
+            int det = 0;
+            Console.WriteLine("");
+            det = m[0, 0] * m[1, 1] - m[0, 1] * m[1, 0];
+            return det;
+        }
+
+        static void MassOut(int[,]m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write("{0,3}", m[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ fall2017/oh_my/matrix determinant n/matrix determinant n/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrix_determinant_n
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter order of your matrix...");
+            int rank = int.Parse(Console.ReadLine());
+            int[,] m = new int[rank, rank];
+            Console.WriteLine("Enter your matrix (line by line with spaces between elements)");
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                string enterString = Console.ReadLine();
+                string[] massiveString = enterString.Split(new Char[] { ' ' });
+                for (int j = 0; j < massiveString.Length; j++)
+                {
+                    m[i, j] = int.Parse(massiveString[j]);
+                }
+            }
+            int det = DetCalc(m);
+            Console.WriteLine("Determinant of your matrix is... {0}",(det));
+            Console.ReadKey();
+        }
+
+        static int Sign(int i, int j)
+        {
+            if ((i + j) % 2 == 0) { return 1; }
+            else { return -1; }
+        }
+        static int[,] CreateSubMat(int[,] m, int i, int j)
+        {
+            int order = int.Parse(System.Math.Sqrt(m.Length).ToString());
+            int[,] result = new int[order - 1, order - 1];
+            int x = 0, y = 0;
+            for (int l = 0; l < order; l++, x++)
+            {
+                if (l != i)
+                {
+                    y = 0;
+                    for (int n = 0; n < order; n++)
+                    {
+                        if (n != j)
+                        {
+                            result[x, y] = m[l, n];
+                            y++;
+                        }
+                    }
+                }
+                else
+                {
+                    x--;
+                }
+            }
+            return result;
+        }
+        
+        static int DetCalc(int[,] m)
+        {
+            int order = int.Parse(System.Math.Sqrt(m.Length).ToString());
+            
+
+            if (order > 2)
+            {
+                int value = 0;
+                for (int j = 0; j < order; j++)
+                {
+                    int[,] temp = CreateSubMat(m, 0, j);
+                    value = value + m[0, j] * (Sign(0, j) * DetCalc(temp));
+                }
+                return value;
+            }
+            else if (order == 2)
+            {
+                return ((m[0, 0] * m[1, 1]) - (m[1, 0] * m[0, 1]));
+            }
+            else
+            {
+                return (m[0, 0]);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
