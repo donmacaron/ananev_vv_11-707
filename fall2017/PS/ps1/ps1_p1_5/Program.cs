2eec2fc754d9ca664c525d3fb819895704c8766a
Don Macaron <donmacaron95@gmail.com>
2017-11-15 00:39:15 +0300

    продам гараж

--- fall2017/PS/ps1/ps1_p1_5/Program.cs
+++ fall2017/PS/ps1/ps1_p1_5/Program.cs
@@ -10,42 +10,31 @@ namespace task1_5
     {
         static void Main(string[] args)
         {
-            int[,] pol = new int[8, 8];
-            //положение ферзя вводится парой координат от 1 до 8
-            Console.WriteLine("Введите положение ферзя (через Enter):");
-            int x = int.Parse(Console.ReadLine());
-            int y = int.Parse(Console.ReadLine());
-            //приводи координаты к виду от 0 до 7
-            x--;
-            y--;
-            //забиваем массив 0
-            for (int i = 0; i < 8; i++)
-                for (int j = 0; j < 8; j++)
-                {
-                    pol[i, j] = 0;
-                }
-            //обозначаем положение ферзя и затем заполняем диогонали 1 в матрице
-            if (Convert.ToBoolean(pol[x, y] = 2))
+            Console.WriteLine("Enter 1st coordinates");
+            int num0 = Console.Read()-48;
+            char inLet0 = (char)Console.Read();
+            Console.ReadLine();
+            Console.WriteLine("Enter 2nd coordinates");
+            int num1 = Console.Read() - 48;
+            char inLet1 = (char)Console.Read();
+            if (InputCheck(num0, num1, inLet0, inLet1))
             {
-                for (int q = x - 1, w = y - 1; (q >= 0) && (w >= 0); q--, w--)
-                    pol[q, w] = 1;
-                for (int q = x - 1, w = y + 1; (q >= 0) && (w < 8); q--, w++)
-                    pol[q, w] = 1;
-                for (int q = x + 1, w = y - 1; (q < 8) && (w >= 0); q++, w--)
-                    pol[q, w] = 1;
-                for (int q = x + 1, w = y + 1; (q < 8) && (w < 8); q++, w++)
-                    pol[q, w] = 1;
+                int let0 = Convert.ToInt32(inLet0) - 96;
+                int let1 = Convert.ToInt32(inLet1) - 96;
+                if ((num0-let0 == 0)&& (num1 - let1 == 0)) { Console.WriteLine("Same"); }
+                else if ((Math.Abs(num0 - let0)== 1)&&(Math.Abs(num1 - let1) == 1)) { Console.WriteLine("Neighbour"); }
+                else { Console.WriteLine("Nothing here"); }
             }
-            //выводим матрицу
-            for (int i = 0; i < 8; i++)
-            {
-                for (int j = 0; j < 8; j++)
-                {
-                    Console.Write("{0} ", pol[i, j]);
-                }
-                Console.WriteLine();
-            }
+            else { Console.WriteLine("something gone wrong"); }
             Console.ReadKey();
+        }
+        static bool InputCheck(int num1, int num2, char let1, char let2)
+        {
+            bool corCheck1 = false;
+            bool corCheck2 = false;
+            if ((((num1 >= 1) & (num1 <= 8)) & (((let1 >= 'A') & (let1 <= 'H')) || ((let1 >= 'a') & (let1 <= 'h'))))) { corCheck1 = true; }
+            if ((((num2 >= 1) & (num2 <= 8)) & (((let2 >= 'A') & (let2 <= 'H')) || ((let2 >= 'a') & (let2 <= 'h'))))) { corCheck2 = true; }
+            return corCheck1 && corCheck2;
         }
     }
 }
---
+++ fall2017/PS/ps2/ps2_p1_1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ps2_p1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter power of  e...");
+            double x = double.Parse(Console.ReadLine());
+   
+            Console.WriteLine("Enter approximation of calculations...");
+            double e = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("e^x = {0}", Sum(x,e));
+            Console.ReadKey();
+        }
+        
+        static double Sum(double x, double e)
+        {
+            double result = 0;
+            double temp = result;
+            int i = 0;
+            do
+            {
+                temp = result;
+                result += (Pow(x, i) / (Factorial(i)));
+                i++;
+            }
+            while ((result - temp) > e);
+            Console.WriteLine(StepsCount(i));
+            return result;
+        }
+
+        static string StepsCount(int steps)
+        {
+            var result = $"Number of steps: {steps}";
+            return result; 
+        }
+
+        static double Factorial(double n)
+        {
+            double result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        static double Pow(double n, int j)
+        {
+            double result=n;
+            if (j > 0)
+            {
+                for (int i = 1; i < j; i++)
+                {
+                    result *= n;
+                }
+                return result;
+            }
+            else if (j == 0) { return 1; }
+
+            return -1;
+        }
+    }
+}
