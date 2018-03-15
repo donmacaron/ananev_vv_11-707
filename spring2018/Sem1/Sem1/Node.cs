a30cc05664f0de8dc16b95aaee0b27a47613599e
Don Macaron <donmacaron95@gmail.com>
2018-03-15 22:09:26 +0300

    duh

---
+++ spring2018/Sem1/Sem1/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem1
+{
+    public class Node<T>
+    {
+        public Node(int data)
+        {
+            Data = data;
+        }
+        public int Data { get; set; }
+        public Node<T> Previous { get; set; }
+        public Node<T> Next { get; set; }
+    }
+}
